1b2e407ee51e99db049949141921517eba669bd8
samuel <fsamuelsmartins@gmail.com>
2021-01-08 09:50:11 -0300

    docker config

--- Main.py
+++ Main.py
@@ -7,8 +7,11 @@ import numpy as np
 import concurrent.futures
 import pandas as pd
 import json
+import time
 
-configs_path = "/opt/project/configurations/Configs.json"
+
+
+configs_path = "/opt/project/user/configs/Configs.json"
 
 with open(configs_path, 'r') as j:
     json_data = json.load(j)
@@ -18,10 +21,10 @@ with open(configs_path, 'r') as j:
     process_qtd = json_data["number_of_process"]
     back_up_percentage = json_data["back_up_percentage"]
 
-images_path = glob.glob(f"{files_path}/*")
+initial_time = time.time()
 
+images_path = glob.glob(f"{files_path}/*")
 files_path_lists = np.array_split(images_path, process_qtd)
-print(files_path_lists)
 result_df = pd.DataFrame()
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
@@ -33,6 +36,12 @@ with concurrent.futures.ProcessPoolExecutor() as executor:
 
 result_df.reset_index(drop=True, inplace=True)
 result_df.index += 1
+
+exit_var = time.time()
+print(exit_var - initial_time)
+
+initial_time = time.time()
+
 # while images_path:
 #     threads_list = []
 #
@@ -54,8 +63,11 @@ result_df.index += 1
 
 # df = ef.extract_face_features(images_path, files_path, "t0")
 faces_data_graph = cg.generate_conections(result_df[["encoding"]], 0.5, True)
+
 G = cg.create_graph(faces_data_graph, plot_graph=False)
+
 G, clustered_df = cm.cluster_cw(G, cg.get_graph_edges_value(G))
 
-for i in G.nodes():
-    print(i, G.nodes()[i]["labels"])
+clustered_df.to_csv(f"user/data/exit_data/result.csv")
+# for i in G.nodes():
+#     print(i, G.nodes()[i]["labels"])
